0354d863418c838ff3084a5d9bbc9768921f76ba
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-01-27 04:21:25 +0900

    텔레포트 정거장ing

--- S2teleportstation18232.py
+++ S2teleportstation18232.py
@@ -1,4 +1,19 @@
+from collections import deque
+
 #N까지의 텔레포트 정거장, 텔레포트 연결 정보 M줄
 N, M = map(int, input().split())
 #만나는 지점 S, 방주가 있는 점 E
 S, E = map(int, input().split())
+
+teleport = [[]for _ in range(N+1)]
+visited = [-1]*(N+1)
+
+for _ in range(M):
+    x, y = map(int, input().split())
+    teleport[x].append(y)
+    teleport[y].append(x)
+
+def bfs(start):
+    queue = deque()
+    queue.append((start, 0))
+
