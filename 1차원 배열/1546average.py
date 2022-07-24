435a98b745fe801fc902d7e1ecb62612d8585780
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-07-25 04:10:08 +0900

    1차원 배열

--- 1차원 배열/1546average.py
+++ 1차원 배열/1546average.py
@@ -1,9 +1,9 @@
-n = int(input())  # 과목 수
-test_list = list(map(int, input().split()))
-max_score = max(test_list)
+n = int(input())  #과목 수
+test_list = list(map(int, input().split())) #시험 점수 리스트
+max_score = max(test_list) #최대값
 
-new_list = []
+new_list = [] #조작할 리스트
 for score in test_list:
-    new_list.append(score/max_score *100)  # 새로운 점수 생성
+    new_list.append(score/max_score *100)  #새로운 점수 만들어서 추가
 test_avg = sum(new_list)/n
 print(test_avg)
