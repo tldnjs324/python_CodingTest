ff4b618ac4fe04d2e8f0a2156820179d756e2118
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-08-29 02:51:04 +0900

    브루트포스

--- 브루트 포스/14225sumofpartialnumber.py
+++ 브루트 포스/14225sumofpartialnumber.py
@@ -1,6 +1,14 @@
-input()
-a=0
-for i in [*sorted(map(int,input().split()))]:
-    if a+1<i:break
-    a+=i
+#수열 s의 크기 n
+n = int(input())
+#수열 S
+s = list(map(int, input().split()))
+
+a = 0
+
+#수열 s를 정렬해서
+for i in [*sorted(s)]:
+    if a+1 < i:
+        break
+    a += i
+
 print(a+1)
