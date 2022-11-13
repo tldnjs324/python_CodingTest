89c9fe32e27f1c72d4634479607ae5c7b6cd0bc7
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-11-13 18:59:38 +0900

    difficult 2

--- ssafy/D2/1926. 간단한 369게임.py
+++ ssafy/D2/1926. 간단한 369게임.py
@@ -1,11 +1,17 @@
 N = int(input())
 num = ['3', '6', '9']
+answer = ''
+
 for i in range(1, N+1):
     cnt = 0
     for j in str(i):
         if(j in num):
             cnt += 1
-    if(cnt > 0):
-        print('-', end=' ')
+    if(cnt == 0):
+        answer += str(i)
     else:
-        print(i, end=' ')
+        for l in range(cnt):
+            answer += '-'
+    answer += ' '
+
+print(answer)
