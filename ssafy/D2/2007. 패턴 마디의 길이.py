1577866baaab891330a67326e28efeff580ed74e
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-11-14 23:58:39 +0900

    difficult 2

---
+++ ssafy/D2/2007. 패턴 마디의 길이.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+
+    text = input()
+    patten = []
+    next_patten = []
+    ans = 0
+    for i in range(11):
+        patten = text[:i]
+        next_patten = text[i:i * 2]
+        # print(patten)
+        # print(next_patten)
+        if i != 0 and patten == next_patten:
+            ans = len(patten)
+            break
+    print('#{} {}'.format(tc, ans))
