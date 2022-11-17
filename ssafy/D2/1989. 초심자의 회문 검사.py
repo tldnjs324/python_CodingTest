6f4295a8a2fec3b160e880562d6de4a470830058
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-11-17 23:50:30 +0900

    difficult 2

---
+++ ssafy/D2/1989. 초심자의 회문 검사.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for t in range(1, T+1) :
+    word = input()
+    for i in range(len(word)//2) :
+        if word[i] == word[-1-i] :
+            answer = 1
+        else :
+            answer = 0
+    print("#{} {}".format(t, answer))
