470a949332411a7beb026232028dbb1f57f70e42
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-11-19 21:54:42 +0900

    difficult 1

--- ssafy/D1/2071평균값 구하기.py
+++ ssafy/D1/2071평균값 구하기.py
@@ -1,18 +1,4 @@
 T = int(input())
-for tt in range(0, T):
-    nums = list(map(int, input().split()))
-    print("#%d %d" % (tt+1, round(sum(nums)/10)))
-
-"""
-
-T = int(input())
-
 for i in range(T):
-    data = list(map(int, input().split()))
-    sum = 0
-    for j in range(10):
-        if data[j]%2 == 1:
-            sum+=data[j]
-
-    print('#' + str(i+1), str(sum))
-"""
+    nums = list(map(int, input().split()))
+    print("#%d %d" % (i+1, round(sum(nums)/10)))
